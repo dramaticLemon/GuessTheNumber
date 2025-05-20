a2befa25ee0780d2be4111fe06786d398d9d4ee3
dramaticLemon <chebandima27@gmail.com>
2025-05-20 16:27:18 +0300

    Replace System print -> Logger Do private method getNumber

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -1,37 +1,39 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class GuessNumber {
     int live = 5;
     private final int endRange = 101;
     Scanner scanner = new Scanner(System.in);
+    Logger logger = Logger.getLogger(GuessNumber.class.getName());
 
     public void game() {
         int guessNumber = getNumber();
-        System.out.println("WELCOME TO THE GAME !\nGuess the number 0 to " + endRange);
+        logger.info("WELCOME TO THE GAME !\nGuess the number 0 to " + (endRange - 1));
         while (live > 0) {
             int inputInteger = getUserInput(scanner);
 
             if (inputInteger == guessNumber) {
-                System.out.println("Winner!");
+                logger.info("Winner!");
                 scanner.close();
                 return;
             } else {
                 if (live == 1) {
-                    System.out.println("Didn't guess.");
+                    logger.info("Didn't guess.");
                 } else {
-                    System.out.print("Didn't guess, try again, ");
+                    logger.info("Didn't guess, try again, ");
                     if (inputInteger > guessNumber) {
-                        System.out.println("you number > guess number.");
+                        logger.info("you number > guess number.");
                     } else {
-                        System.out.println("you number < guess number.");
+                        logger.info("you number < guess number.");
                     }
                 }
             }
             live--;
         }
-        System.out.println("\n========GAME OVER==========");
-        System.out.println("Guess number is:" + guessNumber);
+        logger.info("\n========GAME OVER==========");
+        logger.info("Guess number is:" + guessNumber);
         scanner.close();
     }
 
@@ -42,9 +44,9 @@ public class GuessNumber {
 
     private int getUserInput (Scanner scanner) {
         while (true) {
-            System.out.println("Enter a number: ");
+            logger.info("Enter a number: ");
             if (!scanner.hasNextInt()) {
-                System.out.println("Enter a integer number!");
+                logger.warning("Enter a integer number!");
                 scanner.next();
             } else {
                 return scanner.nextInt();
